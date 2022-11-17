f238260af93056562749379ba930d985fb85ec5f
Kankit40 <Kankit40@gmail.com>
2022-11-17 16:49:39 +0530

    Added: UC3 Ablility to add at the last

--- src/com/bridgelabz/LinkedListCode.java
+++ src/com/bridgelabz/LinkedListCode.java
@@ -23,11 +23,39 @@ public class LinkedListCode {
         head = newNode;
     }
 
+    // Adding the data to the last
+    public void addLast(String data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        Node tempNode = head;
+        while(tempNode.next != null){
+            tempNode= tempNode.next;
+        }
+        tempNode.next = newNode;
+    }
+    //Printing the code
+    void Print() {
+        if (head == null) {                                  // Checking f head is empty or not
+            System.out.println("Linked list is empty...");
+            return;
+        }
+        Node tempNode = head;                                 // Taking temp node
+        while (tempNode != null) {                          // Running the temp node throughout the linked list
+            System.out.print(tempNode.data + "-");         // Printing temp node data
+            tempNode = tempNode.next;                       // Storing next node in temp data
+        }
+    }
+    
     public static void main(String[] args) {
         LinkedListCode obj = new LinkedListCode();
 
-        obj.addFirst("70");
-        obj.addFirst("30");
         obj.addFirst("56");
+        obj.addFirst("30");
+        obj.addFirst("70");
+
+        obj.Print();
     }
 }
